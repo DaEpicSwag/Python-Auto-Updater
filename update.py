a9aa9cda7cd5e66106652b59cd010f9e36167014
DatOneQuietKid <78164990+DaEpicSwag@users.noreply.github.com>
2021-04-16 08:37:45 -0400

    Update update.py

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ with progressbar.ProgressBar(max_value=10) as bar:
     for i in range(10):
         time.sleep(2)
         bar.update(i)
-
+#YOU MUST HAVE YOUR OWN FTP SERVER/ CDN, YOU CAN USE DISCORDS CDN ALSO
 url = 'http://software2life.com/zip/sonicFTP/update.zip'
 
 
